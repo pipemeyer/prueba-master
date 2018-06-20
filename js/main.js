570242496cd4f1dda831901f10a8cada1a2ab02e
pipemeyer <hola@pipemeyer.com>
2018-06-19 22:15:30 -0400

    Desarrollo a prueba master

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+    $scope.dataTable = [ 
+		 { 
+		  "lorem": "xxxxxxxx",
+		  "ipsum": "xxxxx",
+		  "dolor": "xxxx",
+		  "sit": "xxxx",
+		  "amet": "$200.000",
+		  },
+		  { 
+		  "lorem": "xxxxxxxx",
+		  "ipsum": "xxxxx",
+		  "dolor": "xxxx",
+		  "sit": "xxxx",
+		  "amet": "$100.000",
+		  },
+		  { 
+		  "lorem": "xxxxxxxx",
+		  "ipsum": "xxxxx",
+		  "dolor": "xxxx",
+		  "sit": "xxxx",
+		  "amet": "$140.000",
+		  },
+		  { 
+		  "lorem": "xxxxxxxx",
+		  "ipsum": "xxxxx",
+		  "dolor": "xxxx",
+		  "sit": "xxxx",
+		  "amet": "$120.000",
+		  },
+		  { 
+		  "lorem": "xxxxxxxx",
+		  "ipsum": "xxxxx",
+		  "dolor": "xxxx",
+		  "sit": "xxxx",
+		  "amet": "$500.000",
+		  }
+		]
+});
